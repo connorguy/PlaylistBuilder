b1faf604afa47dc954cfb6cc2ca77d3d17cfa352
connorguy <connor_guy@icloud.com>
2018-04-22 12:06:40 -0700

    Basic layout of the server side interaction of app.

---
+++ server.go
@@ -0,0 +1,20 @@
+package main
+
+type userInfo struct {
+	Location string   `json: Location`
+	SongUris []string `json: SongUris`
+}
+
+func main() {
+
+}
+
+// Handles incoming requests from app
+func server() {
+
+}
+
+// Sends song data to Spotify playlist
+func postToSpotify() {
+
+}
